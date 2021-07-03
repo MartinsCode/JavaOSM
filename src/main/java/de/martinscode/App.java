3977c180861bd85b7a2ffed64f845fc40ce1acae
Martin Brunzel <martin.brunzel@gmx.de>
2021-07-03 20:05:17 +0200

    some chages to App.java for good style

--- src/main/java/de/martinscode/App.java
+++ src/main/java/de/martinscode/App.java
@@ -4,10 +4,17 @@ package de.martinscode;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    protected App() {
+        // prevents calls which are not expected
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Entry for the main program.
+     * @param args options from command line
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
     }
 }
