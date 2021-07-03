93612b396d1eb3a954820069ce03d7a29353c121
Martin Brunzel <martin.brunzel@gmx.de>
2021-07-03 19:07:08 +0200

    initialize empty maven project

---
+++ src/main/java/de/martinscode/App.java
@@ -0,0 +1,13 @@
+package de.martinscode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
