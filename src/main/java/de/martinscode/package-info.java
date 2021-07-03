dd8f8ea39831a36537dcf77521cd433eb600c7ce
Martin Brunzel <martin.brunzel@gmx.de>
2021-07-03 20:04:44 +0200

    add package-info-java for javadoc

---
+++ src/main/java/de/martinscode/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * JavaOSM is mostly for fun.
+ * Playground for testing creating windows in java,
+ * getting around with the OSM-API, maybe messing
+ * around with Dijkstra, and last but not least
+ * playing with Github
+ */
+package de.martinscode;
